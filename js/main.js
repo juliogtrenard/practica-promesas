1fed5be5f790879aeefdb6a53d174a961b9cf9ac
Julio González <juliogtrenard@gmail.com>
2025-11-04 15:56:53 +0100

    ADD ejercicio con async

--- js/main.js
+++ js/main.js
@@ -1,41 +1,106 @@
-const contenedorDatos = document.querySelector('.datos');
-const boton  = document.querySelector('.btn');
+// Con promesas
+
+/*
+const contenedorDatos = document.querySelector(".datos");
+const boton = document.querySelector(".btn");
 const usuario = {
-    nombre: "Pepe" ,
-    correo: "pepe@pepe.pepe"
-}
+  nombre: "Pepe",
+  correo: "pepe@pepe.pepe",
+};
 
-boton.addEventListener('click', ()=>{
+boton.addEventListener("click", () => {
+  setTimeout(() => {
     obtenerDatos()
-    .then((respuesta)=>{
+      .then((respuesta) => {
         pintarDatos(respuesta);
-    })
-    .catch((error)=>{
+      })
+      .catch((error) => {
         pintarDatos(error);
-    })
-})
+      });
+  }, 2000);
+});
 
-const obtenerDatos = () =>{
-    const promise = new Promise ((resolve, reject) =>{
-        if(true){
-            resolve(`Nombre: ${usuario.nombre} Correo: ${usuario.correo}`)
-        }else{
-            reject('no hay datos')
-        }
-    });
+const obtenerDatos = () => {
+  const promise = new Promise((resolve, reject) => {
+    if (true) {
+      resolve(`Nombre: ${usuario.nombre} Correo: ${usuario.correo}`);
+    } else {
+      reject("no hay datos");
+    }
+  });
 
-    return promise;
+  return promise;
 };
 
-const pintarDatos = (mensaje) =>{
-    eleminarDatos();
-    const parrafo = document.createElement('P');
-    parrafo.classList.add("parrafo");
-    parrafo.textContent = mensaje;
-    contenedorDatos.append(parrafo);
-
+const pintarDatos = (mensaje) => {
+  eleminarDatos();
+  const parrafo = document.createElement("P");
+  parrafo.classList.add("parrafo");
+  parrafo.textContent = mensaje;
+  contenedorDatos.append(parrafo);
 };
 
-const eleminarDatos =() =>{
-    contenedorDatos.innerHTML = "";
+const eleminarDatos = () => {
+  contenedorDatos.innerHTML = "";
+};
+*/
+
+// Con async
+const contenedorDatos = document.querySelector(".datos");
+const boton = document.querySelector(".btn");
+const usuario = {
+  nombre: "Pepe",
+  correo: "pepe@pepe.pepe",
+};
+
+boton.addEventListener("click", () => {
+  setTimeout(() => {
+    tiempo()
+      .then((response) => pintarDatos(response))
+      .catch((error) => pintarDatos(error));
+  }, 2000);
+});
+
+/**
+ * @description Obtiene los datos del usuario
+ * @returns {Promise<string>} Promesa con los datos del usuario
+ */
+const tiempo = async () => {
+  try {
+    let datos = await obtenerDatos();
+
+    return datos;
+  } catch (error) {
+    throw error;
+  }
+};
+
+/**
+ * @description Obtiene los datos del usuario
+ * @returns {Promise<string>} Promesa con los datos del usuario
+ */
+const obtenerDatos = async () => {
+  if (true) return `Nombre: ${usuario.nombre} Correo: ${usuario.correo}`;
+  else throw `No hay datos`;
+};
+
+/**
+ * @description Pinta los datos en el contenedor
+ * @param {string} mensaje Mensaje a pintar
+ */
+const pintarDatos = (mensaje) => {
+  eliminarDatos();
+
+  const parrafo = document.createElement("P");
+  parrafo.classList.add("parrafo");
+  parrafo.textContent = mensaje;
+  contenedorDatos.append(parrafo);
+};
+
+/**
+ * @description Elimina los datos del contenedor
+ * @returns {void}
+ */
+const eliminarDatos = () => {
+  contenedorDatos.innerHTML = "";
 };
